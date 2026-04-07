1926e60bacf8db86cdbf726414d67ac125510be1
akashgopalasetti <akashgopalasetti85@gmail.com>
2026-04-07 16:54:43 +0530

    Update app.js

--- finance-backend/app.js
+++ finance-backend/app.js
@@ -1,10 +1,9 @@
 const express = require("express");
-
-
-
 const cors = require("cors");
+
 const app = express();
 
+// ✅ CORS
 app.use(cors({
   origin: [
     "http://localhost:3000",
@@ -14,11 +13,12 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
+// body parser
 app.use(express.json());
 
-// Routes
+// routes
 app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/records", require("./routes/recordRoutes"));
 app.use("/api/dashboard", require("./routes/dashboardRoutes"));
 
