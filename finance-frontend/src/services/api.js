e6484988e826864e68bfb917270ef5a834ea9fc1
akashgopalasetti <akashgopalasetti85@gmail.com>
2026-04-07 13:42:37 +0530

    Update api.js

--- finance-frontend/src/services/api.js
+++ finance-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://finance-tracker-1-6184.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
