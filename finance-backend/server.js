f2c515f1d5dc19ef9aeb0a57dae98e79d2b9abc6
akashgopalasetti <akashgopalasetti85@gmail.com>
2026-04-07 16:55:08 +0530

    Refactor server.js to simplify DB connection

--- finance-backend/server.js
+++ finance-backend/server.js
@@ -1,12 +1,7 @@
 const app = require("./app");
-const connectDB = require("./config/db");
-
-require("dotenv").config();
-
-connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port", PORT);
 });
